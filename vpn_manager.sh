5da187fcc6368d729c62f237cc86f4b8ac5ffe8d
Mortyo666 <63021029+Mortyo666@users.noreply.github.com>
2025-11-27 18:44:02 +0800

    Use exec for OVPN menu.py - full terminal control

--- vpn_manager.sh
+++ vpn_manager.sh
@@ -10,7 +10,7 @@ read -r choice </dev/tty
 case $choice in
   1)
     echo "Запуск скрипта создания учёток OVPN..."
-    python /usr/local/openvpn_scripts/menu.py </dev/tty
+    exec python /usr/local/openvpn_scripts/menu.py 
     ;;
   2)
     echo "Создание учёток WireGuard"
